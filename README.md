d0fd691dbdea72017773dd63a4920923f89e3f21
nopnop2002 <hau26280@tree.odn.ne.jp>
2021-12-16 11:09:14 +0900

    First Release

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
 # esp-idf-tea5767
-TEA5767 FM Stereo Radio Driver for esp-idf
+TEA5767 FM Stereo Radio Driver for esp-idf.
+
+I ported from [here](https://github.com/andykarpov/TEA5767).   
+
+# Hardware requirements   
+TEA5767 FM Stereo Radio Module.   
+I bought this on AliExpress about $4.   
+
+![tea5767-1](https://user-images.githubusercontent.com/6020549/146292319-adf96f9a-f076-4b4f-be9f-2a2928c0b92f.JPG)
+![tea5767-2](https://user-images.githubusercontent.com/6020549/146292325-c70aaddb-6f61-45ca-8de3-42ba3f375876.JPG)
+
+The module has a standard antenna, but if you want to use it in a room, you need a long antenna.   
+With a long antenna, you can get more signals.   
+I used an AC power cable.   
+
+![tea5767-3](https://user-images.githubusercontent.com/6020549/146294473-9b514cf8-ca94-49d8-a723-ec67185ec119.JPG)
+
+
+# Installation for ESP32
+
+```
+git clone https://github.com/nopnop2002/esp-idf-tea5767
+cd esp-idf-tea5767
+idf.py set-target esp32
+idf.py menuconfig
+idf.py flash
+```
+
+# Installation for ESP32-S2
+
+```
+git clone https://github.com/nopnop2002/esp-idf-tea5767
+cd esp-idf-tea5767
+idf.py set-target esp32s2
+idf.py menuconfig
+idf.py flash
+```
+
+# Installation for ESP32-C3
+
+```
+git clone https://github.com/nopnop2002/esp-idf-tea5767
+cd esp-idf-tea5767
+idf.py set-target esp32c3
+idf.py menuconfig
+idf.py flash
+```
+
+# Configuration   
+You have to set this config value with menuconfig.   
+- CONFIG_SCL_GPIO   
+ GPIO number(IOxx) to SCL.
+- CONFIG_SDA_GPIO   
+ GPIO number(IOxx) to SDA.
+- CONFIG_FM_BAND   
+ In US/EU it ranges from 87.5 MHz to 108 MHz.   
+ In Japan it ranges from 76 MHz to 91 MHz.   
+ Used when wrapping in a search.   
+
+![config-top](https://user-images.githubusercontent.com/6020549/146292879-4be4bc9b-6a2e-4cb9-b0a8-bdad5fae8615.jpg)
+![config-tea5767](https://user-images.githubusercontent.com/6020549/146292884-e29e45a4-4f99-4314-bb20-4f03bacbe2f7.jpg)
+
+# Wireing
+
+|TEA5767||ESP32|ESP32-S2|ESP32-C3|
+|:-:|:-:|:-:|:-:|:-:|
+|SCL|--|GPIO22|GPIO12|GPIO9|
+|SDA|--|GPIO21|GPIO11|GPIO10|
+|GND|--|GND|GND|GND|
+|VCC|--|3.3V|3.3V|3.3V|
+
+__You can change it to any pin using menuconfig.__   
+__But it may not work with other GPIOs.__
+
+
+# API
+Refer to tea5767.h.   
+
+# How to use   
+
+- Search radio station   
+ Press the + key to search for radio stations upwards.   
+ Press the - key to search for radio stations downwards.   
+
+- Preset radio station   
+ Press the * key to record the current radio station in NVS.   
+ At boot time, it read from NVS and set to the current Radio station.   
+
+- Clear preset   
+ idf.py erase-flash   
+
---
+++ main/main.c
@@ -0,0 +1,298 @@
+/* The example of TEA5767
+ *
+ * This sample code is in the public domain.
+ */
+
+#include <stdio.h>
+#include "math.h"
+#include "string.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+//#include "freertos/queue.h"
+#include "freertos/message_buffer.h"
+#include "nvs_flash.h"
+#include "nvs.h"
+#include "esp_log.h"
+#include "cJSON.h"
+
+#include "tea5767.h"
+
+static char *TAG = "MAIN";
+static char *KEY = "preset_freq";
+
+MessageBufferHandle_t xMessageBufferMain;
+
+void keyin(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+
+	uint16_t c;
+	while (1) {
+		c = fgetc(stdin);
+		if (c == 0xffff) {
+			vTaskDelay(10);
+			continue;
+		}
+		ESP_LOGI(pcTaskGetName(NULL), "c=%x", c);
+		if (c == 0x2b) {
+			ESP_LOGI(TAG, "Push +");
+			cJSON *request;
+			request = cJSON_CreateObject();
+			cJSON_AddStringToObject(request, "id", "searchup-request");
+			char *my_json_string = cJSON_Print(request);
+			ESP_LOGD(TAG, "my_json_string\n%s",my_json_string);
+			size_t xBytesSent = xMessageBufferSend(xMessageBufferMain, my_json_string, strlen(my_json_string), portMAX_DELAY);
+			if (xBytesSent != strlen(my_json_string)) {
+				ESP_LOGE(TAG, "xMessageBufferSend fail");
+			}
+			cJSON_Delete(request);
+			cJSON_free(my_json_string);
+
+		} else if (c == 0x2d) {
+			ESP_LOGI(TAG, "Push -");
+			cJSON *request;
+			request = cJSON_CreateObject();
+			cJSON_AddStringToObject(request, "id", "searchdown-request");
+			char *my_json_string = cJSON_Print(request);
+			ESP_LOGD(TAG, "my_json_string\n%s",my_json_string);
+			size_t xBytesSent = xMessageBufferSend(xMessageBufferMain, my_json_string, strlen(my_json_string), portMAX_DELAY);
+			if (xBytesSent != strlen(my_json_string)) {
+				ESP_LOGE(TAG, "xMessageBufferSend fail");
+			}
+			cJSON_Delete(request);
+			cJSON_free(my_json_string);
+
+		} else if (c == 0x2a) {
+			ESP_LOGI(TAG, "Push *");
+			cJSON *request;
+			request = cJSON_CreateObject();
+			cJSON_AddStringToObject(request, "id", "preset-request");
+			char *my_json_string = cJSON_Print(request);
+			ESP_LOGD(TAG, "my_json_string\n%s",my_json_string);
+			size_t xBytesSent = xMessageBufferSend(xMessageBufferMain, my_json_string, strlen(my_json_string), portMAX_DELAY);
+			if (xBytesSent != strlen(my_json_string)) {
+				ESP_LOGE(TAG, "xMessageBufferSend fail");
+			}
+			cJSON_Delete(request);
+			cJSON_free(my_json_string);
+		}
+	}
+
+	/* Never reach */
+	vTaskDelete( NULL );
+}
+
+esp_err_t NVS_check_key(nvs_handle_t my_handle, char * key) {
+	ESP_LOGD(TAG, "NVS_KEY_NAME_MAX_SIZE=%d", NVS_KEY_NAME_MAX_SIZE);
+	if (strlen(key) > NVS_KEY_NAME_MAX_SIZE-1) {
+		ESP_LOGE(TAG, "Maximal length is %d", NVS_KEY_NAME_MAX_SIZE-1);
+		return ESP_ERR_INVALID_ARG;
+	}
+
+	ESP_LOGI(TAG, "Checking [%s] from NVS ... ", key);
+	int32_t value = 0; // value will default to 0, if not set yet in NVS
+	esp_err_t err = nvs_get_i32(my_handle, key, &value);
+	switch (err) {
+		case ESP_OK:
+			ESP_LOGI(TAG, "Done. [%s] = %d", key, value);
+			break;
+		case ESP_ERR_NVS_NOT_FOUND:
+			ESP_LOGW(TAG, "The value is not initialized yet!");
+			break;
+		default :
+			ESP_LOGE(TAG, "Error (%s) reading!", esp_err_to_name(err));
+			break;
+	}
+	return err;
+}
+
+int32_t NVS_read_key(nvs_handle_t my_handle, char * key) {
+	ESP_LOGD(TAG, "NVS_KEY_NAME_MAX_SIZE=%d", NVS_KEY_NAME_MAX_SIZE);
+	if (strlen(key) > NVS_KEY_NAME_MAX_SIZE-1) {
+		ESP_LOGE(TAG, "Maximal length is %d", NVS_KEY_NAME_MAX_SIZE-1);
+		return ESP_ERR_INVALID_ARG;
+	}
+
+	ESP_LOGI(TAG, "Reading [%s] from NVS ... ", key);
+	int32_t value = 0; // value will default to 0, if not set yet in NVS
+	esp_err_t err = nvs_get_i32(my_handle, key, &value);
+	switch (err) {
+		case ESP_OK:
+			ESP_LOGI(TAG, "Done. [%s] = %d", key, value);
+			break;
+		case ESP_ERR_NVS_NOT_FOUND:
+			ESP_LOGW(TAG, "The value is not initialized yet!");
+			break;
+		default :
+			ESP_LOGE(TAG, "Error (%s) read!", esp_err_to_name(err));
+			break;
+	}
+	return value;
+}
+
+esp_err_t NVS_write_key(nvs_handle_t my_handle, char * key, int32_t value) {
+	ESP_LOGD(TAG, "NVS_KEY_NAME_MAX_SIZE=%d", NVS_KEY_NAME_MAX_SIZE);
+	if (strlen(key) > NVS_KEY_NAME_MAX_SIZE-1) {
+		ESP_LOGE(TAG, "Maximal length is %d", NVS_KEY_NAME_MAX_SIZE-1);
+		return ESP_ERR_INVALID_ARG;
+	}
+
+	esp_err_t err = nvs_set_i32(my_handle, key, value);
+	ESP_LOGI(TAG, "nvs_set_i32 err=%d", err);
+	if (err == ESP_OK) {
+		// Commit written value.
+		// After setting any values, nvs_commit() must be called to ensure changes are written
+		// to flash storage. Implementations may write to storage at other times,
+		// but this is not guaranteed.
+		ESP_LOGI(TAG, "Committing updates in NVS ... ");
+		err = nvs_commit(my_handle);
+		ESP_LOGI(TAG, "nvs_commit err=%d", err);
+		if (err != ESP_OK) {
+			ESP_LOGE(TAG, "Error (%s) commit!", esp_err_to_name(err));
+		}
+	} else {
+		ESP_LOGE(TAG, "Error (%s) write!", esp_err_to_name(err));
+	}
+	return err;
+}
+
+esp_err_t NVS_delete_key(nvs_handle_t my_handle, char * key) {
+	ESP_LOGD(TAG, "NVS_KEY_NAME_MAX_SIZE=%d", NVS_KEY_NAME_MAX_SIZE);
+	if (strlen(key) > NVS_KEY_NAME_MAX_SIZE-1) {
+		ESP_LOGE(TAG, "Maximal length is %d", NVS_KEY_NAME_MAX_SIZE-1);
+		return ESP_ERR_INVALID_ARG;
+	}
+
+	esp_err_t err = nvs_erase_key(my_handle, key);
+	ESP_LOGI(TAG, "nvs_erase_key err=%d", err);
+	if (err != ESP_OK) {
+		ESP_LOGE(TAG, "Error (%s) erase!", esp_err_to_name(err));
+	}
+	return err;
+}
+
+void app_main()
+{
+	// Initialize NVS
+	esp_err_t err = nvs_flash_init();
+	if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		// NVS partition was truncated and needs to be erased
+		// Retry nvs_flash_init
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		err = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK( err );
+
+	// Open NVS
+	ESP_LOGI(TAG, "Opening Non-Volatile Storage handle");
+	nvs_handle_t my_handle;
+	err = nvs_open("storage", NVS_READWRITE, &my_handle);
+	if (err != ESP_OK) {
+		ESP_LOGE(TAG, "Error (%s) opening NVS handle", esp_err_to_name(err));
+		while(1) { vTaskDelay(1); }
+	}
+	ESP_LOGI(TAG, "nvs_open Done");
+
+	int32_t preset_frequence = 0;
+	// Check key
+	err = NVS_check_key(my_handle, KEY);
+	ESP_LOGI(TAG, "NVS_check_key=%d", err);
+	if (err == ESP_OK) {
+		// Read
+		preset_frequence = NVS_read_key(my_handle, KEY);
+		ESP_LOGI(TAG, "preset_frequence=%d", preset_frequence);
+	}
+
+	// Create Message Buffer
+	xMessageBufferMain = xMessageBufferCreate(1024);
+	configASSERT( xMessageBufferMain );
+	//xQueueCmd = xQueueCreate( 1, sizeof(CMD_t) );
+	//configASSERT( xQueueCmd );
+
+	// Create Task
+	xTaskCreate(keyin, "KEYIN", 1024*4, NULL, 2, NULL);
+
+	TEA5767_t ctrl_data;
+	ESP_LOGI(TAG, "CONFIG_SDA_GPIO=%d",CONFIG_SDA_GPIO);
+	ESP_LOGI(TAG, "CONFIG_SCL_GPIO=%d",CONFIG_SCL_GPIO);
+	radio_init(&ctrl_data, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO);
+
+	double current_freq;
+#if CONFIG_FM_BAND_US
+	if (preset_frequence != 0) {
+		current_freq = preset_frequence / 10.0;
+	} else {
+		current_freq = TEA5767_US_FM_BAND_MIN; // go to station 87.5MHz
+	}
+#endif
+#if CONFIG_FM_BAND_JP
+	radio_set_japanese_band(&ctrl_data);
+	if (preset_frequence != 0) {
+		current_freq = preset_frequence / 10.0;
+	} else {
+		current_freq = TEA5767_JP_FM_BAND_MIN; // go to station 76.0MHz
+	}
+#endif
+	ESP_LOGI(TAG, "current_freq=%f", current_freq);
+	radio_set_frequency(&ctrl_data, current_freq);
+
+	int search_mode = 0;
+	int search_direction = 0;
+	unsigned char buf[5];
+	char cRxBuffer[512];
+
+	while(1) {
+		size_t readBytes = xMessageBufferReceive(xMessageBufferMain, cRxBuffer, sizeof(cRxBuffer), 1000/portTICK_PERIOD_MS);
+		if (readBytes == 0) {
+			//radio_read_status(&ctrl_data, buf);
+			if (radio_read_status(&ctrl_data, buf) == 1) {
+				//double current_freq =	floor (radio_frequency_available (&ctrl_data, buf) / 100000 + .5) / 10;
+				current_freq = floor (radio_frequency_available (&ctrl_data, buf) / 100000 + .5) / 10;
+				int stereo = radio_stereo(&ctrl_data, buf);
+				int signal_level = radio_signal_level(&ctrl_data, buf);
+				ESP_LOGI(TAG, "current_freq=%f stereo=%d signal_level=%d/15", current_freq, stereo, signal_level);
+			}
+
+			if (search_mode == 1) {
+				ESP_LOGI(TAG, "search_direction=%d", search_direction);
+				if (radio_process_search (&ctrl_data, buf, search_direction) == 1) {
+					ESP_LOGI(TAG, "process_search success");
+					search_mode = 0;
+				} else {
+					ESP_LOGW(TAG, "process_search fail");
+				}
+			}
+		} else {
+			ESP_LOGD(TAG, "cRxBuffer=[%.*s]", readBytes, cRxBuffer);
+			cJSON *root = cJSON_Parse(cRxBuffer);
+			if (cJSON_GetObjectItem(root, "id")) {
+				char *id = cJSON_GetObjectItem(root,"id")->valuestring;
+				ESP_LOGI(TAG, "id=[%s]",id);
+				if ( strcmp (id, "searchup-request") == 0) {
+					search_mode = 1;
+					search_direction = TEA5767_SEARCH_DIR_UP;
+					radio_search_up(&ctrl_data, buf);
+				}
+
+				if ( strcmp (id, "searchdown-request") == 0) {
+					search_mode = 1;
+					search_direction = TEA5767_SEARCH_DIR_DOWN;
+					radio_search_down(&ctrl_data, buf);
+				}
+
+				if ( strcmp (id, "preset-request") == 0) {
+					preset_frequence = current_freq * 10;
+					err = NVS_write_key(my_handle, KEY, preset_frequence);
+					ESP_LOGI(TAG, "NVS_write_key=%d, preset_frequence=%d current_freq=%f", err, preset_frequence, current_freq);
+				}
+
+			} // end if
+			// Delete a cJSON structure
+			cJSON_Delete(root);
+		}
+	} // end while
+
+	// Close NVS
+	nvs_close(my_handle);
+}
